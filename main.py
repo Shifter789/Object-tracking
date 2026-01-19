a62d93b1ed7936857ae37cab82711cdc9dc4017d
percy <percygandhi211@gmail.com>
2026-01-18 23:20:31 -0600

    Project created

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+#libraries
 import cv2
+import numpy as np
 
 
 #varibles
 cam = cv2.VideoCapture(0) #cam
 
+#fuctions
 def start_feed():
     while True:
         ret, frame = cam.read()
         if not ret:
             break
 
-        cv2.imshow("Camera", frame)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("frame", gray)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cam.release()
     cv2.destroyAllWindows()
-
 
 def main():
     start_feed()
