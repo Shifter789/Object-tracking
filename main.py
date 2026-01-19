6b23e1b4d69754664dd112bf31b7469612f06dbb
percy <percygandhi211@gmail.com>
2026-01-19 12:26:16 -0600

    Project created

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
-#libraries
+# libraries
 import cv2
 import numpy as np
 
+# variables
+cam = cv2.VideoCapture(0)  # cam
 
-#varibles
-cam = cv2.VideoCapture(0) #cam
 
-#fuctions
+# functions
 def start_feed():
+    show_gray = False  # Variable to toggle between color and grayscale
+    show_detection = False
+
     while True:
         ret, frame = cam.read()
         if not ret:
             break
-        cv2.imshow("Cam", frame)
+
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("frame", gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+
+        if show_gray:
+            cv2.imshow("Cam", gray)
+        else:
+            cv2.imshow("Cam", frame)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        #color and grayscale toggle
+        if key == ord('g'):
+            show_gray = not show_gray
+
+        #Quit
+        if key == ord('q'):
             break
+
     cam.release()
     cv2.destroyAllWindows()
 
 
 def main():
     start_feed()
+
 
 if __name__ == '__main__':
     main()
