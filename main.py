b1dce087fa0d3e1c28d14b5fd76b465fbdc6824a
percy <percygandhi211@gmail.com>
2026-01-19 00:27:46 -0600

    Project created

--- main.py
+++ main.py
@@ -12,19 +12,17 @@ def start_feed():
         ret, frame = cam.read()
         if not ret:
             break
-
+        cv2.imshow("Cam", frame)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow("frame", gray)
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
     cam.release()
     cv2.destroyAllWindows()
 
+
 def main():
     start_feed()
-
 
 if __name__ == '__main__':
     main()
