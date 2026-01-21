989ae8e88d11f90739e8ff85ecedb789940b2b55
percy <percygandhi211@gmail.com>
2026-01-20 18:49:52 -0600

    Project created

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 # libraries
 import cv2
 import numpy as np
+from kasa import Device
+import asyncio
 
 # variables
 cam = cv2.VideoCapture(0)  # cam
 
 
 # functions
+async def toggle_dimmer(): #lights
+    switch = await Device.connect(host="10.0.0.159")
+    await switch.update()
+    if switch.is_on:
+        await switch.turn_off()
+        print("Switch turned off")
+    else:
+        await switch.turn_on()
+        print("Switch turned on")
+
 def start_feed():
     show_gray = False  # Variable to toggle between color and grayscale
     show_detection = False
@@ -28,7 +40,9 @@ def start_feed():
         #color and grayscale toggle
         if key == ord('g'):
             show_gray = not show_gray
-
+            #lights
+        if key == ord('l'):
+            asyncio.run(toggle_dimmer())
         #Quit
         if key == ord('q'):
             break
@@ -39,7 +53,6 @@ def start_feed():
 
 def main():
     start_feed()
-
 
 if __name__ == '__main__':
     main()
